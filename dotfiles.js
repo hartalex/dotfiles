db14735a155f01ae20cfe7178e2352dc5824b75d
Alex Hart <hartalex@gmail.com>
2019-09-16 21:04:39 -0500

    feat(dotfiles.js): Working on handling file copies and git repos

--- dotfiles.js
+++ dotfiles.js
@@ -3,8 +3,11 @@ const fs = require("fs");
 const util = require("util");
 const path = require("path");
 const colors = require("colors");
+const crypto = require("crypto");
+const git = require("nodegit");
 
 const configFileName = "dotfilesrc.json";
+const repoPath = ".";
 const {
   env: { HOME: homePath }
 } = process;
@@ -18,25 +21,95 @@ const { files } = JSON.parse(fileContents);
 // TODO: validate Config
 
 const errors = files.map(file => {
-  // Find File
+  const { name } = file;
+  // Find destination File
   const filePath = findFile(file);
   if (!filePath) {
-    return { message: "Could not find file", reason: "file-not-found", file };
+    return {
+      message: "Could not find in locations",
+      reason: "file-not-found",
+      file
+    };
   }
-  console.log(colors.blue(filePath));
+  const repoFilePath = path.join(repoPath, name);
+  // Compare local files against filePath
+  if (!filesAreSame(repoFilePath, filePath)) {
+    //copy filePath to repoFilePath
+    fs.copyFileSync(filePath, repoFilePath);
+    return {
+      message: "Files are different",
+      reason: "file-diff",
+      file
+    };
+  }
+
   return undefined;
 });
 
-errors.map(error => {
-  if (!error) {
-    return;
-  }
-  const {
-    message,
-    file: { name }
-  } = error;
-  console.error(colors.red(`${message} : ${name}`));
+const filteredErrors = errors.filter(error => error);
+
+const fileDiffErrors = filteredErrors.filter(error => {
+  const { reason } = error;
+  return reason === "file-diff";
 });
+
+if (fileDiffErrors.length > 0) {
+  const fileNameList = fileDiffErrors.reduce((acc, error, idx) => {
+    const {
+      file: { name }
+    } = error;
+    if (idx === 0) {
+      return name;
+    }
+    return `${acc}, ${name}`;
+  }, "");
+  console.warn(
+    colors.blue(
+      `Found differences in file(s): ${fileNameList}. Please Review, Commit, and Push these to the repo.`
+    )
+  );
+}
+git.Repository.open(repoPath)
+  .then(repo => {
+    return repo.getStatus();
+  })
+  .then(statusList => {
+    if (statusList.length > 0) {
+      const fileNameList = statusList.reduce((acc, status, idx) => {
+        if (idx === 0) {
+          return status.path();
+        }
+        return `${acc}, ${status.path()}`;
+      }, "");
+
+      console.warn(
+        colors.blue(
+          `There are files that still need to be committed: ${fileNameList}`
+        )
+      );
+      return undefined;
+    }
+    return repo;
+  })
+  .then(repo => {
+    if (repo) {
+      return repo.fetch();
+    }
+    filteredErrors.map(error => {
+      const {
+        message,
+        reason,
+        file: { name }
+      } = error;
+      if (reason !== "file-diff") {
+        console.error(colors.red(`${message} : ${name}`));
+      }
+    });
+  })
+  .then(() => {})
+  .catch(error => {
+    console.error(colors.red(util.inspect(error)));
+  });
 
 function findFile(file) {
   const { name, locations } = file;
@@ -69,5 +142,24 @@ function fileExists(filePath) {
     // TODO: Change to debug log
     console.error(colors.red(util.inspect(error)));
   }
+  return false;
+}
+
+function checksum(str, algorithm, encoding) {
+  return crypto
+    .createHash(algorithm || "sha512")
+    .update(str, "utf8")
+    .digest(encoding || "hex");
+}
+
+function filesAreSame(a, b) {
+  const aData = fs.readFileSync(a);
+  const aHash = checksum(aData);
+  const bData = fs.readFileSync(b);
+  const bHash = checksum(bData);
+  if (aHash === bHash) {
+    return true;
+  }
+
   return false;
 }
